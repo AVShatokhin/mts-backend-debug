2e403c27f75d0df975336394b0abee8820a9d788
AVShatokhin <andrey.v.shatokhin@gmail.com>
2021-12-07 16:31:15 +0300

    Добавлена загрузка фала с результатом

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fileUpload = require("express-fileupload");
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -12,6 +13,7 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(fileUpload({ safeFileNames: false }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,6 +30,8 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
+  console.log(req);
+  console.log(err.message);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/upload_file", function (req, res, next) {
+  // console.log(req.files.new_result.mimetype);
+  // console.log(req);
+
+  req.files.new_result.mv("mts-results/new_result.zip");
+  res.json({ result: "ok" });
 });
 
 module.exports = router;
